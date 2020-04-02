90a75e44a1e269e0b80578d0bbbc7da82ce40518
Angelica Velez <anvelez26@gmail.com>
2020-04-01 21:46:10 -0500

    scaffolding complete

---
+++ Flowers.Core/Entities/Store.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Flowers.Core.Entities
+{
+    public partial class Store
+    {
+        public Store()
+        {
+            Inventory = new HashSet<Inventory>();
+            Order = new HashSet<Order>();
+        }
+
+        public int StoreId { get; set; }
+        public string StoreName { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public int PostalCode { get; set; }
+
+        public virtual ICollection<Inventory> Inventory { get; set; }
+        public virtual ICollection<Order> Order { get; set; }
+    }
+}
