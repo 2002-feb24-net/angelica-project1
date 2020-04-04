ce5ddadf877b3823426fa0af3397651bb5c8ffab
Angelica Velez <anvelez26@gmail.com>
2020-04-03 22:27:48 -0500

    made secretconfig/user secrets, added interface and repository

---
+++ Flowers/Flowers.Core/Interface/IFlowersRepository.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Flowers.Core.Model;
+
+
+namespace Flowers.Core.Interface
+{
+    public class IFlowersRepository
+    {
+        // Customer GetCustomerById(string Username);
+
+
+
+    }
+}
---
+++ Flowers/Flowers.Infra/Repositories/FlowersRepository.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Text;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using Flowers.Core.Interface;
+using Flowers.Infra.Entities;
+
+namespace Flowers.Infra.Repositories
+{
+    public class FlowersRepository : IFlowersRepository
+    {
+
+    }
+}
