50a80480830ec2b8d5a3ccd098cf6582cf50c3ae
Angelica Velez <anvelez26@gmail.com>
2020-04-04 14:30:30 -0500

    interface and repository made for customers

--- Flowers/Flowers.Core/Interface/IFlowersRepository.cs
+++ Flowers/Flowers.Core/Interface/IFlowersRepository.cs
@@ -6,11 +6,25 @@ using Flowers.Core.Model;
 
 namespace Flowers.Core.Interface
 {
-    public class IFlowersRepository
+    public interface IFlowersRepository
     {
-        // Customer GetCustomerById(string Username);
+         // get all customers with deffered execution
+        IEnumerable<Customer> GetCustomers(string search = null);
 
+        // get a customer by ID
+        Customer GetCustomerById(string Username);
 
+       // Add a customer
+        void AddCustomer (Customer customer );
+
+        // update a customer
+        void UpdateCustomer(Customer customer);
+
+        // // add an order and associate it with a customer
+        // void AddOrder(Order order, Customer customer = null);
+
+        // persist changes to the data resource
+        void Save();
 
     }
 }
--- Flowers/Flowers.Infra/Repositories/FlowersRepository.cs
+++ Flowers/Flowers.Infra/Repositories/FlowersRepository.cs
@@ -4,11 +4,35 @@ using System.Linq;
 using Microsoft.EntityFrameworkCore;
 using Flowers.Core.Interface;
 using Flowers.Infra.Entities;
+using System.Collections.Generic;
 
 namespace Flowers.Infra.Repositories
 {
     public class FlowersRepository : IFlowersRepository
     {
+        public void AddCustomer(Core.Model.Customer customer)
+        {
+            throw new NotImplementedException();
+        }
 
+        public Core.Model.Customer GetCustomerById(string Username)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Core.Model.Customer> GetCustomers(string search = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Save()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateCustomer(Core.Model.Customer customer)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
