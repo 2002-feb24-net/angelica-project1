eb9cdae1bc5715f64215ae98e057d09a8c3eb197
Angelica Velez <anvelez26@gmail.com>
2020-04-04 22:48:38 -0500

    added view

--- Flowers/Flowers.WebUI/Controllers/CustomerController.cs
+++ Flowers/Flowers.WebUI/Controllers/CustomerController.cs
@@ -2,20 +2,43 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.ModelBinding;
-using Flowers.Core.Interface;
 using Flowers.Core.Model;
 using Flowers.WebUI.Models;
+using Microsoft.EntityFrameworkCore;
+using Flowers.Infra.Entities;
 
 namespace Flowers.WebUI.Controllers
 {
     public class CustomerController : Controller
     {
+        private readonly FlowersContext _context;
+
+        public CustomerController(FlowersContext context)
+        {
+            _context = context;
+        }
+
         public IActionResult Index()
         {
+            return View(_context.Customer.ToList());
+        }
+        public IActionResult Create()
+        {
             return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create([Bind("CustomerId, FirstName, LastName, Username")] Core.Model.Customer customer)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(customer);
+                _context.SaveChanges();
+            };
+            return View(customer);
         }
     }
 }
--- Flowers/Flowers.WebUI/Controllers/HomeController.cs
+++ Flowers/Flowers.WebUI/Controllers/HomeController.cs
@@ -33,5 +33,8 @@ namespace Flowers.WebUI.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
+
+
     }
 }
