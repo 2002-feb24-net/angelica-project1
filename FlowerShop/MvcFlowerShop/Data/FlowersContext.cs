cc08fe50ebe5ecde19b85ad7947b039b824f0b84
Angelica Velez <anvelez26@gmail.com>
2020-04-05 21:28:25 -0500

    connection string modified in startup/json

--- FlowerShop/MvcFlowerShop/Data/FlowersContext.cs
+++ FlowerShop/MvcFlowerShop/Data/FlowersContext.cs
@@ -25,7 +25,7 @@ namespace MvcFlowerShop.Models
         {
             if (!optionsBuilder.IsConfigured)
             {
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+// #warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
                 optionsBuilder.UseSqlServer("Server=tcp:2002-training-velez.database.windows.net,1433;Initial Catalog=Flowers;Persist Security Info=False;User ID=angelica;Password=Password123;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
             }
         }
--- FlowerShop/MvcFlowerShop/Startup.cs
+++ FlowerShop/MvcFlowerShop/Startup.cs
@@ -8,6 +8,8 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using MvcFlowerShop.Data;
+using Microsoft.EntityFrameworkCore;
 
 namespace MvcFlowerShop
 {
@@ -24,6 +26,9 @@ namespace MvcFlowerShop
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+
+           services.AddDbContext<FlowersContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("FlowersContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
