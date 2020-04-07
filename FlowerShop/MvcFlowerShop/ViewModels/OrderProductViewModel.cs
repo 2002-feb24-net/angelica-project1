39c9b1f6a198ce60ce7fac83762efef389126547
Angelica Velez <anvelez26@gmail.com>
2020-04-07 02:10:33 -0500

    fixed dropdown menu

--- FlowerShop/MvcFlowerShop/ViewModels/OrderProductViewModel.cs
+++ FlowerShop/MvcFlowerShop/ViewModels/OrderProductViewModel.cs
@@ -15,17 +15,17 @@ namespace MvcFlowerShop.ViewModels
     {
         public List<Customer> Customers { get; set; } = new List<Customer>();
         [Display(Name ="Choose a customer")]
-        public int[] CustomerChoice { get; set; }
+        public int CustomerChoice { get; set; }
         public IEnumerable<SelectListItem> CustomerList { get; set; }
 
         public List<Store> Stores { get; set; } = new List<Store>();
         [Display(Name ="Choose a store location")]
-        public int[] LocationChoice { get; set; }
+        public int LocationChoice { get; set; }
         public IEnumerable<SelectListItem> LocationList { get; set; }
 
         public List<Product> Products { get; set; } = new List<Product>();
         [Display(Name ="Choose a flower")]
-        public int[] ProductChoice { get; set; }
+        public int ProductChoice { get; set; }
         public IEnumerable<SelectListItem> ProductList { get; set; }
 
 
